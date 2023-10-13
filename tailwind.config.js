9c8d5b7e7948752f4677adf49d1aef7a25413e04
Enielect <eniolafaithful57@gmail.com>
2023-10-13 21:12:20 +0100

    fixed tailwind bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,41 @@ export default {
   theme: {
     extend: {
       height: {
+        124: "20rem",
+        126: "22rem",
         128: "32rem",
+        150: "36rem",
         300: "55rem",
+        122: "40rem",
+      },
+      width: {
+        124: "20rem",
+        128: "32rem",
+        126: "22rem",
+        150: "36rem",
+        300: "55rem",
+        122: "40rem",
       },
       fontFamily: {
         barlow: ["Barlow"],
         fraunces: ["Fraunces"],
         gabarito: ["Gabarito"],
-        youngSerif: ['Young Serif']
+        youngSerif: ["Young Serif"],
       },
       colors: {
         footer: "#90D4C5",
         footerText: "hsl(167, 40%, 24%)",
+        thirdText: "hsl(198, 62%, 26%)",
       },
+      letterSpacing: {
+        widestt: ".25em",
+      },
+      lineHeight: {
+        high: "3.5rem",
+      },
+      // padding: {
+      //   14.5: ''
+      // }
     },
   },
   plugins: [],
